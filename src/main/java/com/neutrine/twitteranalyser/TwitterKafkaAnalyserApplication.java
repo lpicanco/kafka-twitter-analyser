f5e20a97dc3820b270a45c08f98908bf78692f65
Luiz Picanco <lpicanco@gmail.com>
2016-01-26 21:36:23 -0200

    Removing storm module.

--- src/main/java/com/neutrine/twitteranalyser/TwitterKafkaAnalyserApplication.java
+++ src/main/java/com/neutrine/twitteranalyser/TwitterKafkaAnalyserApplication.java
@@ -1,12 +1,7 @@
 package com.neutrine.twitteranalyser;
 
-import backtype.storm.generated.AlreadyAliveException;
-import backtype.storm.generated.AuthorizationException;
-import backtype.storm.generated.InvalidTopologyException;
 import com.neutrine.twitteranalyser.adapter.TwitterAdapter;
-import com.neutrine.twitteranalyser.consumer.KafkaConsumer;
 import com.neutrine.twitteranalyser.producer.KafkaProducer;
-import com.neutrine.twitteranalyser.storm.WordCountTopology;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -21,27 +16,12 @@ public class TwitterKafkaAnalyserApplication implements CommandLineRunner {
     @Autowired
     private KafkaProducer kafkaProducer;
 
-    @Autowired
-    private WordCountTopology wordCountTopology;
-
-
     public static void main(String[] args) {
         SpringApplication.run(TwitterKafkaAnalyserApplication.class, args);
     }
 
     @Override
     public void run(String... strings) throws Exception {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    wordCountTopology.execute();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-
         twitterAdapter.execute(kafkaProducer);
     }
 }
